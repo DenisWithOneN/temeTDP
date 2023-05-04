112c7737f51c1f4eac91566d7fea50dafb244b44
DenisWithOneN <denis.ursut@emanuel.com>
2023-05-04 11:45:10 +0300

    finished table

--- chessTable/main.js
+++ chessTable/main.js
@@ -15,29 +15,3 @@ function draw() {
 
 
 }
-
-function initSquares() {
-
-    let rows = 8;
-    let columns = 8;
-
-    for(i = 0; i<columns; i++)
-        for(j = 0; j<rows; j++){
-
-            let chessSquare = {
-
-                x: j * 50 + 50,
-                y: i * 50 + 50,
-                size: 50,
-            };
-            chessTable.push(chessSquare);
-        }
-}
-
-function createSquares() {
-
-    for (let i = 0; i < chessTable.length; i++) {
-        let Square = chessTable[i];
-        square(Square.x, Square.y, Square.size);
-    }
-}
--- chessTable/table.js
+++ chessTable/table.js
@@ -1,25 +1,44 @@
 function initSquares() {
+  let rows = 8;
+  let columns = 8;
+  let c1;
+  let c2;
 
-    let rows = 8;
-    let columns = 8;
+  for (i = 0; i < rows; i++) {
+    if (i % 2 == 0) {
+      c1 = 255;
+      c2 = 0;
+    } else {
+      c1 = 0;
+      c2 = 255;
+    }
 
-    for(i = 0; i<columns; i++)
-        for(j = 0; j<rows; j++){
-
-            let chessSquare = {
-
-                x: j * 50 + 50,
-                y: i * 50 + 50,
-                size: 50,
-            };
-            chessTable.push(chessSquare);
-        }
+    for (j = 0; j < columns; j++) {
+      if (j % 2 == 0) {
+        let chessSquare = {
+          x: j * 50 + 50,
+          y: i * 50 + 50,
+          size: 50,
+          color: color(c1, c1, c1),
+        };
+        chessTable.push(chessSquare);
+      } else {
+        let chessSquare = {
+          x: j * 50 + 50,
+          y: i * 50 + 50,
+          size: 50,
+          color: color(c2, c2, c2),
+        };
+        chessTable.push(chessSquare);
+      }
+    }
+  }
 }
 
 function createSquares() {
-
-    for (let i = 0; i < chessTable.length; i++) {
-        let Square = chessTable[i];
-        square(Square.x, Square.y, Square.size);
-    }
+  for (let i = 0; i < chessTable.length; i++) {
+    let Square = chessTable[i];
+    fill(Square.color);
+    square(Square.x, Square.y, Square.size);
+  }
 }
